6d67a1df288641b1b75f688fd2f3acdd67961895
Miguel Laria <miguellaria33@gmail.com>
2026-03-10 10:53:14 +0000

    Add ProductServiceTest

--- product-service/src/test/java/com/academy/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/academy/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,20 @@ package com.academy.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
+	@Test
+	void productNameIsValid() {
+		String name = "Laptop";
+		assertNotNull(name);
+		assertFalse(name.isBlank());
+	}
 }
